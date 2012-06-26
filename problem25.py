4382b951fe66cc93342b03b9d17ba3268d91f648
Tom <tom.emelko@gmail.com>
2012-06-26 16:06:52 -0400

    Finished problem 25

--- problem25.py
+++ problem25.py
@@ -1,6 +1,6 @@
 import myfunc
 
-fib = myfunc.fibonacci(1,1,10**1001)
+fib = myfunc.fibonacci(1,0,10**1000)
 for i in fib:
-  if (len(str(i)) > 1000):
-    print fib.index(i)
+  if (len(str(i)) == 1000):
+    print fib.index(i),len(str(i))
