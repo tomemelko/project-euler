e432988c9d1086b448cdc6c8519bfead745258f0
Tom <tom.emelko@gmail.com>
2012-06-26 16:18:41 -0400

    Added problem 21

---
+++ problem21.py
@@ -0,0 +1,13 @@
+from myfunc import *
+
+list = []
+for i in range(1,10000):
+  if (i not in list):
+    if (i == sum(divisors(sum(divisors(i)))):
+      list.append(i)
+      list.append(sum(divisors(i))
+      
+    
+  
+print list
+print sum(list)
