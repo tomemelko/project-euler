b00da64e77e3fb6a3ab0e5e26fc32b4a47a98aae
Tom <tom.emelko@gmail.com>
2012-06-26 14:25:57 -0400

    Finished problem 3

--- problem3.py
+++ problem3.py
@@ -1,6 +1,8 @@
 import math
 from myfunc import is_prime
 
-#for i in range(3,int(math.sqrt(600851475143)),2):
-for i in range(3,int(math.sqrt(13195)),2):
- print i,is_prime(i)
+n = 600851475143
+for i in range(3,int(math.sqrt(n)),2):
+  if (n % i == 0):
+    if (is_prime(i)):
+      print i
