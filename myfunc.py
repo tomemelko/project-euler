dbe2ae8a02d4114bd0dbe41d9cb36bdac7f87441
Tom <tom.emelko@gmail.com>
2012-06-26 16:58:11 -0400

    Added perfect and abudant number generators

--- myfunc.py
+++ myfunc.py
@@ -18,7 +18,32 @@ def is_prime(n):
 def fibonacci(n1, n2, limit):
   list = []
   while n2 < limit:
+    #print "Adding ",n2
+    #print "Is the", len(list), "th term with length", len(str(n2))
     list.append(n2)
     n1, n2 = n2, n1+n2
 
   return list
+  
+def divisors(n):
+  list = []
+  for i in range(1,n):
+    if (n % i == 0):
+      list.append(i)
+      
+    
+  return list
+
+def perfect_numbers(n):
+  list = []
+  for i in range(1,n):
+    if (sum(divisors(i)) == i):
+      list.append(i)
+
+
+
+def abundant_numbers(n):
+  list = []
+  for i in range(1,n):
+    if (sum(divisors(i)) > i):
+      list.append(i)
