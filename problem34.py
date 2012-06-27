af1a9da9f00d0dc87ef2353153b528838ae49ae6
Tom <tom.emelko@gmail.com>
2012-06-27 14:25:19 -0400

    Added problem 34

---
+++ problem34.py
@@ -0,0 +1,15 @@
+from math import facotrial
+
+total = 0
+
+for i in range(3,200)
+  sum = 0
+  for d in str(i)
+    sum += factorial(int(d))
+  if (sum == i)
+    print i,"is awesome"
+    total += i
+    
+  
+
+print total
