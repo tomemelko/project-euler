ea0d35de1711a4379537fac09bcc422aca76a24e
Tom <tom.emelko@gmail.com>
2012-06-27 14:16:27 -0400

    Sped up finished problem 30

--- problem30.py
+++ problem30.py
@@ -1,7 +1,7 @@
 power = 5
 total = 0
 
-for i in range(2,1000000):
+for i in range(2,200000): #limit is 200,000 because that's greater than the largest such number, only changed to improve running time
   sum = 0
   stri = str(i)
   for d in stri:
