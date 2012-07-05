f72f16de63d235b7244763e237eb73e21a35e839
tomemelko <tom.emelko@gmail.com>
2012-07-05 15:24:56 -0400

    Finished problem 5

--- problem005.py
+++ problem005.py
@@ -1,7 +1,3 @@
-for i in xrange(0,500000000):
-  ans = True
-  for j in xrange(1,20):
-    if (i % j != 0):
-      ans = False
-  if ans:
-    print i
+import myfunc
+
+print myfunc.lcm(range(2,20))    
