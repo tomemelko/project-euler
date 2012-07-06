0c8002e072ca63759387722cdeebefd83d301f3c
tomemelko <tom.emelko@gmail.com>
2012-07-06 11:29:18 -0400

    Added a Collatz term counter

--- myfunc.py
+++ myfunc.py
@@ -107,3 +107,13 @@ def collatz(n):
     else:
       n = (3*n)+1
     yield n
+
+def collatz_count(n):
+  count = 0
+  while (n != 1):
+    if (n%2==0):
+      n = n/2
+    else:
+      n = (3*n)+1
+    count += 1
+  return count      
