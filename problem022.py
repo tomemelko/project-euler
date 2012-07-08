67e57ba10f002225c3a0aaf57a6eb5802af5e046
tomemelko <tom.emelko@gmail.com>
2012-07-08 16:01:07 -0400

    Finished problem 22

--- problem022.py
+++ problem022.py
@@ -1,3 +1,10 @@
-names = []
-for i in open('p22names.txt', 'r'):
-  names.append(i)
+names = open('p22names.txt' , 'r').read().replace('"', '').split(',')
+names.sort()
+sum = 0
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for n, i in enumerate(names):
+  namescore = 0
+  for letter in i:
+    namescore += alphabet.index(letter) + 1
+  sum += (n + 1) * namescore
+print sum
