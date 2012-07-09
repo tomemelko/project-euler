310095cdbfc840c90959e1c04ad90bff5c36cbba
tomemelko <tom.emelko@gmail.com>
2012-07-09 13:55:55 -0400

    Finished problem 55

--- problem055.py
+++ problem055.py
@@ -1,8 +1,12 @@
 from myfunc import is_palindrome
 
-for i in xrange(10):
+count = 0
+for i in xrange(10000):
   iterCount = 0
-  num = i
-  while (is_palindrome(num) == False and iterCount < 51):
-    print num
+  num = int(str(i)[::-1]) + i #First step must happen outside of loop, so that we can avoid initial palindromes
+  while (is_palindrome(str(num)) == False and iterCount < 51):
     num = int(str(num)[::-1]) + num
+    iterCount += 1
+  if iterCount > 50:
+    count += 1
+print count
