c546e228b393cafacc20b7877b94d13f05f44310
tomemelko <tom.emelko@gmail.com>
2012-07-09 11:27:16 -0400

    Changed from using the math module

--- myfunc.py
+++ myfunc.py
@@ -8,7 +8,7 @@ def is_prime(n):
     return True
   if (n % 2 == 0):
     return False
-  for i in range(3, int(math.sqrt(n))+1, 2):
+  for i in range(3, int(n**.5)+1, 2):
     if (n % i == 0):
       return False
   return True
