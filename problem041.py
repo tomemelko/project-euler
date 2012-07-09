bb11592078fe4ff42d7371e1abeb27abd108be5c
tomemelko <tom.emelko@gmail.com>
2012-07-09 10:01:30 -0400

    Finished problem 41

--- problem041.py
+++ problem041.py
@@ -1,8 +1,12 @@
-from myfunc import is_prime, is_pandigital
+from myfunc import is_prime
+from itertools import permutations
 max = 0
-for i in xrange(1000000001,1,-2):
-  if (is_pandigital(i, len(str(i)))):
-    if (is_prime(i)):
-    #if (is_pandigital(i, len(str(i)))):
-      print i
-      exit()
+#Since all 9-pandigital numbers will sum to 45, and therefore are divisible by 3
+#And the samething goes for 8 digit pandigital numbers, as they sum to 36
+#We only need to go up to 7 digits
+for i in permutations(['1','2','3','4','5','6','7']):
+  num = ''.join(i)
+  if (is_prime(int(num))):
+    if (num > max):
+      max = num
+print max
