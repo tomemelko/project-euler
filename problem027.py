d5458280e5a6c3b33ef0add0ed7d7122dfa1bda4
tomemelko <tom.emelko@gmail.com>
2012-07-09 11:32:43 -0400

    Brute force solution, not sure if works

--- problem027.py
+++ problem027.py
@@ -3,11 +3,13 @@ from myfunc import is_prime
 bestcount = 0
 bestproduct = 0
 for n in xrange(0,200):
+  print "n=",n
   for a in xrange(-1000,1000):
     count = 0
     for b in xrange(-1000,1000):
-      if (is_prime((n**2)+(a*n)+b)):
-        count += 1
+      if (((n**2)+(a*n)+b) > 0):
+        if (is_prime((n**2)+(a*n)+b)):
+          count += 1
     if (count > bestcount):
       bestcount, bestproduct = count, a*b
-print best product
+print bestproduct
