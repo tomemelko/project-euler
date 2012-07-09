07e69e4d8ca647d3cb789b5be54d8386730ac3b0
tomemelko <tom.emelko@gmail.com>
2012-07-09 12:24:09 -0400

    Brute force, doesn' t work

--- problem027.py
+++ problem027.py
@@ -1,4 +1,4 @@
-from myfunc import is_prime
+from myfunc import is_prime, prime_list
 
 bestcount = 0
 bestproduct = 0
@@ -6,7 +6,7 @@ for n in xrange(0,200):
   print "n=",n
   for a in xrange(-1000,1000):
     count = 0
-    for b in xrange(-1000,1000):
+    for b in prime_list(1000):
       if (((n**2)+(a*n)+b) > 0):
         if (is_prime((n**2)+(a*n)+b)):
           count += 1
