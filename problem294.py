c4e4a96bb15b14d2e2b8e39b929143bdce361f39
tomemelko <tom.emelko@gmail.com>
2012-07-10 15:18:09 -0400

    Streamlined, still very slow

--- problem294.py
+++ problem294.py
@@ -1,18 +1,12 @@
 from myfunc import sum_of_digits
 
 def s(n):
-  isNum = True
   count = 0
-  for k in xrange(10**n):
+  for k in xrange(2300,10**n,23):
     if (k % 10**(n-2) == 0):
       print k
-    if (k % 23 != 0):
-      isNum = False
-    if (isNum):
-      if (sum_of_digits(k) != 23):
-        isNum = False
-    if (isNum):
-      count += 0
+    if (sum_of_digits(k) == 23):
+      count += 1
   return count
 
 print s(9)
