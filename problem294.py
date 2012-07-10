19d75955e5a070a6a1d2a1384f662f94c6d73e77
tomemelko <tom.emelko@gmail.com>
2012-07-10 16:16:23 -0400

    Added time complexity analysis

--- problem294.py
+++ problem294.py
@@ -1,18 +1,23 @@
+#Time complexity: O(len(n))
 def test(n):
   total = 0
   while (n != 0):
     if (total > 23):
-      return False
+      return False  
     total, n = total + n%10,n/10
   if (total == 23):
     return True
   return False
 
+#Time complexity: O((10^n)*(len(k))
+#Since len(k) = n in the worst case (which is also the best case in this implementation)
+#Time complexity: O(n*(10^n)) 
+#Exponential time... There has got to be a better way to do this.
 def s(n):
   count = 0
   for k in xrange(2300,10**n,23):
-    if (k % 10**(n-2) == 0):
-      print float(k)/10**(n-2)
+    #if (k % 10**(n-2) == 0):
+      #print float(k)/10**(n-2)
     if (test(k)):
       count += 1
   return count
