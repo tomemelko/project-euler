bd6e8b43fd975a6dc74c9e85426b1cccd16d1bb9
tomemelko <tom.emelko@gmail.com>
2012-07-10 15:50:29 -0400

    Improved speed, still SUPER slow

--- problem294.py
+++ problem294.py
@@ -1,11 +1,19 @@
-from myfunc import sum_of_digits
+def test(n):
+  total = 0
+  while (n != 0):
+    if (total > 23):
+      return False
+    total, n = total + n%10,n/10
+  if (total == 23):
+    return True
+  return False
 
 def s(n):
   count = 0
   for k in xrange(2300,10**n,23):
     if (k % 10**(n-2) == 0):
-      print k
-    if (sum_of_digits(k) == 23):
+      print float(k)/10**(n-2)
+    if (test(k)):
       count += 1
   return count
 
